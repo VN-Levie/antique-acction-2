937067d0b1f665500a00943d4cbdb9d180f27f0a
thiên lê <thienle255@gmail.com>
2023-07-13 01:16:30 +0700

    Update ContactsUss.vue

--- resources/js/Components/Home/ContactsUss.vue
+++ resources/js/Components/Home/ContactsUss.vue
@@ -1,17 +1,27 @@
 <template>
+  <div class="row">
+    <div class="col-12 d-flex justify-content-center">
+      <span class="title_team">Team</span>
+    </div>
 
+    <div class="col-12 d-flex justify-content-center">
+      <img
+        src="../../../../public/img/mt-1804-home-divider1.png"
+        alt="divider1"
+      />
+    </div>
+  </div>
+
+  
 </template>
 
 <script>
 export default {
-  setup () {
-    
-
-    return {}
-  }
-}
+  setup() {
+    return {};
+  },
+};
 </script>
 
 <style>
-
 </style>
