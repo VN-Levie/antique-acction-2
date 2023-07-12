87c7e79f18742c00cf8fb129b96ee003d3de315e
Dai Vi <quangdaivi2303@gmail.com>
2023-07-12 19:47:10 +0700

    changes

--- resources/js/Components/Home/About.vue
+++ resources/js/Components/Home/About.vue
@@ -1,77 +1,23 @@
 <template>
-  <div
-    id="About"
-    class="moto-widget moto-widget-block moto-spacing-top-large moto-spacing-right-auto moto-spacing-bottom-large moto-spacing-left-auto"
-  >
-    <a class="moto-anchor" name="about"></a>
-    <div class="container-fluid">
+  <div id="About" class="spacing-bottom ">
+    <div class="">
       <div class="row">
-        <div class="moto-cell col-sm-12">
-          <div
-            class="moto-widget moto-widget-row row-fixed moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto"
-          >
-            <div class="container-fluid">
-              <div class="row">
-                <div
-                  class="moto-widget moto-widget-row__column moto-cell col-sm-12 moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto"
-                >
-                  <div
-                    class="moto-widget moto-widget-text moto-preset-default moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-small moto-spacing-left-auto"
-                  >
-                    <div
-                      class="moto-widget-text-content moto-widget-text-editable"
-                    >
-                      <p class="text moto-text_system_8">
-                        RESTORERS, SPECIALISTS
-                      </p>
-                      <h2 class="text moto-text_system_5">
-                        We Are Professional
-                      </h2>
-                    </div>
-                  </div>
-
-                  <div
-                    class="moto-widget moto-widget-image moto-widget_with-deferred-content moto-preset-default moto-align-center moto-spacing-top-small moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto"
-                  >
-                    <span class="moto-widget-image-link">
-                      <img
-                        src="/img/mt-1804-home-divider2.png"
-                        class="moto-widget-image-picture moto-widget-deferred-content lazyload"
-                      />
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
+        <p class="text moto-text_system_8">
+          RESTORERS, SPECIALISTS
+        </p>
+        <h2 class="text moto-text_system_5">
+          We Are Professional
+        </h2>
+        <div class="">
+          <div class="moto-widget-image moto-preset-default moto-align-center moto-spacing-top-small">
+            <span class="moto-widget-image-link">
+              <img src="/img/mt-1804-home-divider2.png" class="" />
+            </span>
           </div>
-          <div
-            class="moto-widget moto-widget-row row-fixed moto-spacing-top-medium moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto"
-          >
-            <div class="container-fluid">
-              <div class="row">
-                <div
-                  class="moto-widget moto-widget-row__column moto-cell col-sm-1 moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto"
-                ></div>
-                <div
-                  class="moto-widget moto-widget-row__column moto-cell col-sm-10 moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto"
-                >
-                  <div
-                    class="moto-widget moto-widget-text moto-preset-default moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto"
-                  >
-                    <div
-                      class="moto-widget-text-content moto-widget-text-editable"
-                    >
-                      <p class="text moto-text_system_9">
-                        {{ title }}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div
-                  class="moto-widget moto-widget-row__column moto-cell col-sm-1 moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto"
-                ></div>
-              </div>
-            </div>
+          <div class=" container-fluid moto-spacing-top-medium col-sm-1 col-sm-10">
+            <h3 class="text1 moto-text_system_9">
+              {{ title }}
+            </h3>
           </div>
         </div>
       </div>
@@ -98,7 +44,25 @@ export default {
   background-repeat: no-repeat;
   background-size: contain;
 }
-.text {
+
+.moto-spacing-bottom-large1 {
+  padding-bottom: 95px;
+}
+
+.text1 {
   text-align: center;
+
+}
+
+.text1 {
+  text-align: center;
+  font-weight: 700;
+  color: rgb(132, 132, 132);
+}
+
+.spacing-bottom {
+  padding-bottom: 266px;
+  padding-top: 158px;
+
 }
 </style>
--- resources/js/Components/Home/Sliders.vue
+++ resources/js/Components/Home/Sliders.vue
@@ -1,65 +1,24 @@
 <template>
-  <div
-    class="slider moto-widget moto-widget-block moto-spacing-top-large moto-spacing-right-auto moto-spacing-bottom-large moto-spacing-left-auto"
-  >
-    <div class="container-fluid">
-      <div class="row">
-        <div class="moto-cell col-sm-12">
-          <div
-            class="moto-widget moto-widget-row row-fixed moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto"
-          >
-            <div class="container-fluid">
-              <div class="row">
-                <div
-                  class="moto-widget moto-widget-row__column moto-cell col-sm-12 moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto"
-                >
-                  <div
-                    class="moto-widget moto-widget-spacer moto-preset-default moto-spacing-top-medium moto-spacing-right-auto moto-spacing-bottom-medium moto-spacing-left-auto moto-visible-on_mobile-h_hidden moto-visible-on_mobile-v_hidden"
-                  >
-                    <div class="height moto-widget-spacer-block"></div>
-                  </div>
-                  <div
-                    class="moto-widget moto-widget-text moto-preset-default moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-small moto-spacing-left-auto"
-                  >
-                    <div
-                      class="moto-widget-text-content moto-widget-text-editable"
-                    >
-                      <p class="text moto-text_system_12">ANTIQUE SERVICES</p>
-                      <h1 class="text moto-text_system_4">
-                        Specializing in Antiques &amp; Collectibles
-                      </h1>
-                    </div>
-                  </div>
-                  <div
-                    class="moto-widget moto-widget-image moto-widget_with-deferred-content moto-preset-default moto-align-center moto-spacing-top-small moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto"
-                  >
-                    <span class="moto-widget-image-link">
-                      <img
-                        src="/img/mt-1804-home-divider3.png"
-                        class="moto-widget-image-picture moto-widget-deferred-content lazyload"
-                      />
-                    </span>
-                  </div>
-                  <div
-                    class="moto-widget moto-widget-spacer moto-preset-default moto-spacing-top-large moto-spacing-right-auto moto-spacing-bottom-medium moto-spacing-left-auto moto-visible-on_mobile-h_hidden moto-visible-on_mobile-v_hidden"
-                  >
-                    <div class="height moto-widget-spacer-block"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+  <div class="slider moto-widget moto-spacing-top-large moto-spacing-bottom-large moto-widget-row row-fixed   ">
+    <div class="moto-preset-default moto-spacing-top-medium  moto-spacing-bottom-medium ">
+      <div class="moto-preset-default moto-spacing-top-medium  moto-spacing-bottom-medium ">
+        <div class="height moto-widget-spacer-block"></div>>
+        <div class="container-fluid">
+          <p class="text moto-text_system_12">ANTIQUE SERVICES</p>
+          <h1 class="text moto-text_system_4">
+            Specializing in Antiques &amp; Collectibles
+          </h1>
+        </div>
+        <div
+          class="moto-widget moto-widget-image moto-widget_with-deferred-content moto-preset-default moto-align-center moto-spacing-top-small moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto">
+          <span class="moto-widget-image-link">
+            <img src="/img/mt-1804-home-divider3.png"
+              class="moto-widget-image-picture moto-widget-deferred-content lazyload" />
+          </span>
         </div>
       </div>
     </div>
   </div>
-
-  <div
-    data-widget-id="wid_1556101667_53fgqdczn"
-    class="moto-widget moto-widget-actions-open-popup"
-    data-widget="actions.open_popup"
-    data-actions-open-popup-options='{"actionId":"wid_1556101667_53fgqdczn_1556101667940","popupId":7,"triggerType":"timer","delayTime":3000,"recurrenceCondition":"always","recurrenceOptions":{"shows":1,"overtime":86400}}'
-  ></div>
 </template>
 
 <script>
@@ -81,6 +40,7 @@ export default {
 .text {
   text-align: center;
 }
+
 .height {
   height: 10px;
 }
