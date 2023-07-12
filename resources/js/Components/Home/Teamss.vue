87d91b3f64773e42549b1e3ff0d954c71781b02c
thiên lê <thienle255@gmail.com>
2023-07-13 00:52:08 +0700

    Update Teamss.vue

--- resources/js/Components/Home/Teamss.vue
+++ resources/js/Components/Home/Teamss.vue
@@ -1,86 +1,74 @@
 <template>
-  <div
-    class="moto-widget moto-widget-block moto-spacing-top-large moto-spacing-right-auto moto-spacing-bottom-large moto-spacing-left-auto"
-  >
-    <a class="moto-anchor" name="team"></a>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="moto-cell col-sm-12">
-          <div
-            class="moto-widget moto-widget-row row-fixed moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto"
-          >
-            <div class="container-fluid">
-              <div class="row" data-container="container">
-                <div
-                  class="moto-widget moto-widget-row__column moto-cell col-sm-12 moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto"
-                >
-                  <div
-                    class="moto-widget moto-widget-text moto-preset-default moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-small moto-spacing-left-auto"
-                  >
-                    <div
-                      class="moto-widget-text-content moto-widget-text-editable"
-                    >
-                      <p style="text-align: center" class="moto-text_system_8">
-                        PROFESSIONALS
-                      </p>
-                      <h2 style="text-align: center" class="moto-text_system_5">
-                        Our Team
-                      </h2>
-                    </div>
-                  </div>
+  <div class="row">
+    <div class="col-12 d-flex justify-content-center">
+      <span class="title_team">Team</span>
+    </div>
 
-                  <div
-                    class="moto-widget moto-widget-image moto-widget_with-deferred-content moto-preset-default moto-align-center moto-spacing-top-small moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto"
-                  >
-                    <span class="moto-widget-image-link">
-                      <img
-                        src="/img/mt-1804-home-divider2.png"
-                        class="moto-widget-image-picture moto-widget-deferred-content lazyload"
-                      />
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+    <div class="col-12 d-flex justify-content-center">
+      <img
+        src="../../../../public/img/mt-1804-home-divider1.png"
+        alt="divider1"
+      />
+    </div>
+  </div>
 
-          <div
-            class="moto-widget moto-widget-row row-fixed moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto"
-          >
-            <div class="container-fluid">
-              <div class="row" data-container="container">
-                <div
-                  class="moto-widget moto-widget-row__column moto-cell col-sm-4 moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto"
-                >
-                  <div
-                    class="moto-widget moto-widget-image moto-widget_with-deferred-content moto-preset-default moto-align-center moto-spacing-top-medium moto-spacing-right-auto moto-spacing-bottom-small moto-spacing-left-auto"
-                  >
-                    <span class="moto-widget-image-link">
-                      <img
-                        src="/img/mt-1804-home-team1.jpg"
-                        class="moto-widget-image-picture moto-widget-deferred-content lazyload"
-                      />
-                    </span>
-                  </div>
-
-                  <div
-                    class="moto-widget moto-widget-text moto-preset-default moto-spacing-top-small moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto"
-                  >
-                    <div
-                      class="moto-widget-text-content moto-widget-text-editable"
-                    >
-                      <h3 style="text-align: center" class="moto-text_system_7">
-                        Christian Hall
-                      </h3>
-                      <p style="text-align: center" class="moto-text_system_9">
-                        <em>Antique Furniture Master</em>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+  <div class="row-team">
+    <div class="column-team">
+      <div class="card-team">
+        <div class="img-container">
+          <img src="../../../../public/img/team1.jpg" alt="" />
+        </div>
+        <h3>Tlee</h3>
+        <p>Design</p>
+        <div class="icons">
+          <a href="#"> <font-awesome-icon :icon="['fab', 'facebook']" /> </a
+          ><a href="#"> <font-awesome-icon :icon="['fab', 'github']" /> </a
+          ><a href="#"> <font-awesome-icon :icon="['fass', 'envelope']" /> </a>
+        </div>
+      </div>
+    </div>
+    <!-- 2 -->
+    <div class="column-team">
+      <div class="card-team">
+        <div class="img-container">
+          <img src="../../../../public/img/team1.jpg" alt="" />
+        </div>
+        <h3>Chuong</h3>
+        <p>Design</p>
+        <div class="icons">
+          <a href="#"> <font-awesome-icon :icon="['fab', 'facebook']" /> </a
+          ><a href="#"> <font-awesome-icon :icon="['fab', 'github']" /> </a
+          ><a href="#"> <font-awesome-icon :icon="['fass', 'envelope']" /> </a>
+        </div>
+      </div>
+    </div>
+    <!-- 3 -->
+    <div class="column-team">
+      <div class="card-team">
+        <div class="img-container">
+          <img src="../../../../public/img/team1.jpg" alt="" />
+        </div>
+        <h3>Duong</h3>
+        <p>Leader</p>
+        <div class="icons">
+          <a href="#"> <font-awesome-icon :icon="['fab', 'facebook']" /> </a
+          ><a href="#"> <font-awesome-icon :icon="['fab', 'github']" /> </a
+          ><a href="#"> <font-awesome-icon :icon="['fass', 'envelope']" /> </a>
+        </div>
+      </div>
+    </div>
+    <!-- 4 -->
+    <div class="column-team">
+      <div class="card-team">
+        <div class="img-container">
+          <img src="../../../../public/img/team1.jpg" alt="" />
+        </div>
+        <h3>Vi</h3>
+        <p>VJ</p>
+        <div class="icons">
+          <a href="#"> <font-awesome-icon :icon="['fab', 'facebook']" /> </a
+          ><a href="#"> <font-awesome-icon :icon="['fab', 'github']" /> </a
+          ><a href="#"> <font-awesome-icon :icon="['fass', 'envelope']" /> </a>
         </div>
       </div>
     </div>
@@ -96,4 +84,106 @@ export default {
 </script>
 
 <style>
+.title_team {
+  font-weight: 700;
+  font-style: normal;
+  font-family: "Libre Baskerville", serif;
+  color: #292929;
+  font-size: 55px;
+  line-height: 1.4;
+  letter-spacing: 0px;
+}
+.row-team {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 2em 1em;
+  text-align: center;
+  box-sizing: border-box;
+}
+
+.column-team {
+  width: 100%;
+  padding: 0.5em 0;
+}
+
+.card-team {
+  box-shadow: 0 0 2.2em rgba(25, 0, 58, 0.1);
+  padding: 3.5em 1em;
+  border-radius: 0.6em;
+  color: #1f003b;
+  cursor: pointer;
+  transition: 0.3s;
+  background-color: #ffffff;
+}
+
+.card-team .img-container {
+  width: 8em;
+  height: 8em;
+  background-color: #a993ff;
+  padding: 0.5em;
+  border-radius: 50%;
+  margin: 0 auto 2em auto;
+}
+
+.card-team img {
+  width: 100%;
+  border-radius: 50%;
+}
+
+.card-team h3 {
+  font-weight: 500;
+}
+
+.card-team p {
+  font-weight: 300;
+  text-transform: uppercase;
+  margin: 0.5em 0 2em 0;
+  letter-spacing: 2px;
+}
+
+.icons {
+  width: 50%;
+  min-width: 180px;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+}
+
+.card-team a {
+  text-decoration: none;
+  color: inherit;
+  font-size: 1.4em;
+}
+
+.card-team:hover {
+  background: linear-gradient(
+    #6045ea, #8567f7
+  );
+  color: #ffffff;
+}
+
+.card-team:hover .img-container {
+  transform: scale(1.15);
+}
+
+@media screen and (min-width: 768px) {
+  ::selection {
+    padding: 1em 7em;
+  }
+}
+
+@media screen and (min-width: 992px) {
+  ::selection {
+    padding: 1em;
+  }
+  .card-team {
+    padding: 5em 1em;
+  }
+
+  .column-team {
+    flex: 0 0 25%;
+    max-width: 25%;
+    padding: 0 1em;
+  }
+}
 </style>
