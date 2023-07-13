bba0f64be487563d82fbd03adbdd9becdf6b2c36
ThanhChuong1 <thanhchuong1412@gmail.com>
2023-07-13 18:40:25 +0700

    Update works.vue

--- resources/js/Components/Home/works.vue
+++ resources/js/Components/Home/works.vue
@@ -14,53 +14,49 @@
               <h2 style="text-align: center" class="moto-text_system_5">
                 Our Best Works
               </h2>
-
               <div
-                class="moto-widget moto-widget-image moto-preset-default moto-align-center moto-spacing-top-small"
-              >
+                class="moto-widget moto-widget-image moto-preset-default moto-align-center moto-spacing-top-small">
                 <span class="moto-widget-image-link">
                   <img src="/img/mt-1804-home-divider2.png" />
                 </span>
               </div>
-              <div
-                class="moto-widget moto-widget-tabs moto-spacing-top-medium moto-preset-default"
-              >
-                <div
-                  class="moto-widget-tabs__headers-wrapper moto-widget-tabs__headers-wrapper_headers-center"
-                >
-                  <div
-                    class="moto-widget-tabs__header moto-widget-tabs__header_desktop moto-widget-tabs__header_opened"
-                  >
-                    <span
-                      class="moto-widget-tabs__header-item moto-widget-tabs__header-title"
-                      >NEW ARRIVALS</span
-                    >
+
+
+
+              <div class="moto-widget moto-widget-tabs moto-spacing-top-medium moto-preset-default">
+                <div class="moto-widget-tabs__headers-wrapper moto-widget-tabs__headers-wrapper_headers-center">
+                  <div class="moto-widget-tabs__header moto-widget-tabs__header_desktop moto-widget-tabs__header_opened">
+                    <span class="moto-widget-tabs__header-item moto-widget-tabs__header-title"
+                      data-toggle="tab" href="#new"
+                      >NEW ARRIVALS</span>
                   </div>
-                  <div
-                    class="moto-widget-tabs__header moto-widget-tabs__header_desktop"
-                  >
-                    <span
-                      class="moto-widget-tabs__header-item moto-widget-tabs__header-title"
-                      >POPULAR</span
-                    >
+
+                  <div class="moto-widget-tabs__header moto-widget-tabs__header_desktop">
+                    <span class="moto-widget-tabs__header-item moto-widget-tabs__header-title" data-toggle="tab" href="#pop">
+                      POPULAR
+                      </span>
                   </div>
                 </div>
+
                 <div class="moto-widget-tabs__header_mobile">
                   <span>NEW ARRIVALS</span>
                 </div>
+
+                <div class="tab-content">
+                <div id="new">
+                <div class="moto-widget moto-widget-block moto-spacing-top-large moto-spacing-right-auto moto-spacing-bottom-large moto-spacing-left-auto" >
                 <div class="container-fluid">
                   <div class="row">
-                    <div class="col-sm-3">
-                      <div class="moto-spacing-top-medium">
-                        <span>
+                    <div class="  col-md-3 "    >
+                        <div class="center">
+                        <span >
                           <img src="/img/mt-1804-home-works1.png" />
                         </span>
                       </div>
-                      <div>
+                      <div class="moto-widget-text-content moto-widget-text-editable">
                         <h3
                           style="text-align: center"
-                          class="moto-text_system_7"
-                        >
+                          class="moto-text_system_7">
                           French Empire figural
                         </h3>
                         <p
@@ -72,6 +68,42 @@
                       </div>
                     </div>
                   </div>
+                </div>
+              </div>
+                </div>
+              <div class="moto-widget-tabs__header_mobile">
+                  <span>Poppular</span>
+                </div>
+                <div id="pop">
+                <div class="moto-widget moto-widget-block moto-spacing-top-large moto-spacing-right-auto moto-spacing-bottom-large moto-spacing-left-auto" >
+                <div class="container-fluid">
+                  <div class="row">
+                    
+                    <div class="  col-md-3 "    >
+                        <div class="center">
+                        <span >
+                          <img src="/img/mt-1804-home-works1.png" />
+                        </span>
+                      </div>
+
+                      <div class="moto-widget-text-content moto-widget-text-editable">
+                        <h3
+                          style="text-align: center"
+                          class="moto-text_system_7">
+                          Thanh Chuio
+                        </h3>
+                        <p
+                          style="text-align: center"
+                          class="moto-text_system_13"
+                        >
+                          $80.00
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+                </div>
                 </div>
               </div>
             </div>
