030d2c5922e77afd9a82ad33e476f66a9c2a1a3d
thiên lê <thienle255@gmail.com>
2023-07-14 00:05:18 +0700

    building components

--- resources/js/Components/Home/Slider10.vue
+++ resources/js/Components/Home/Slider10.vue
@@ -1,71 +1,13 @@
-<!-- <template>
-  <div
-    class="moto-widget moto-widget-block moto-bg-color2_3 moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="moto-cell col-sm-12">
-          <div
-            class="moto-widget moto-widget-row row-fixed moto-justify-content_center moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-small moto-spacing-left-auto">
-            <div class="container-fluid">
-              <div class="row">
-                <div
-                  class="moto-widget moto-widget-row__column moto-cell col-sm-3 moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto">
-                  <div
-                    class="moto-widget moto-widget-image moto-widget_with-deferred-content moto-preset-default moto-align-center_mobile-h moto-spacing-top-small moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto">
-                    <a href="/" class="moto-widget-image-link moto-link">
-                      <img src="/img/mt-1804-home-logo-footer.png"
-                        class="moto-widget-image-picture moto-widget-deferred-content lazyload" />
-                    </a>
-                  </div>
-                </div>
-                <div
-                  class="moto-widget moto-widget-row__column moto-cell col-sm-6 moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto">
-                  <div
-                    class="moto-widget moto-widget-social-links-extended moto-preset-3 moto-align-center moto-spacing-top-small moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto">
-                    <ul class="moto-widget-social-links-extended__list">
-                      <li class="moto-widget-social-links-extended__item moto-widget-social-links-extended__item-1">
-                        <a href="#" class="moto-widget-social-links-extended__link" target="_self">
-                          <span class="moto-widget-social-links-extended__icon fa">&#xf09a;</span>
-                        </a>
-                      </li>
-                      <li class="moto-widget-social-links-extended__item moto-widget-social-links-extended__item-2">
-                        <a href="#" class="moto-widget-social-links-extended__link" target="_self">
-                          <span class="moto-widget-social-links-extended__icon fa">&#xf099;</span>
-                        </a>
-                      </li>
-                      <li class="moto-widget-social-links-extended__item moto-widget-social-links-extended__item-3">
-                        <a href="#" class="moto-widget-social-links-extended__link" target="_self">
-                          <span class="moto-widget-social-links-extended__icon fa">&#xf231;</span>
-                        </a>
-                      </li>
-                      <li class="moto-widget-social-links-extended__item moto-widget-social-links-extended__item-4">
-                        <a href="#" class="moto-widget-social-links-extended__link" target="_self">
-                          <span class="moto-widget-social-links-extended__icon fa">&#xf0e1;</span>
-                        </a>
-                      </li>
-                      <li class="moto-widget-social-links-extended__item moto-widget-social-links-extended__item-5">
-                        <a href="#" class="moto-widget-social-links-extended__link" target="_self">
-                          <span class="moto-widget-social-links-extended__icon fa">&#xf0d5;</span>
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div
-                  class="moto-widget moto-widget-row__column moto-cell col-sm-3 moto-spacing-top-auto moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto">
-                  <div
-                    class="moto-widget moto-widget-text moto-preset-default moto-spacing-top-small moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto">
-                    <div class="moto-widget-text-content moto-widget-text-editable">
-                      <p class="moto-text_system_10" style="text-align: center">
-                        Copyright © 2020. All Rights Reserved
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+<template>
+  <div class="logo-slider">
+    <div class="logos">
+      <div class="logos-slide">
+        <img src="../../../../public/img/logobrand1.jpg" alt="aaa" />
+        <img src="../../../../public/img/logobrand2.jpg" alt="aaa" />
+        <img src="../../../../public/img/logobrand3.jpg" alt="aaa" />
+        <img src="../../../../public/img/logobrand1.jpg" alt="aaa" />
+        <img src="../../../../public/img/logobrand2.jpg" alt="aaa" />
+        <img src="../../../../public/img/logobrand3.jpg" alt="aaa" />
       </div>
     </div>
   </div>
@@ -74,11 +16,66 @@
 <script>
 export default {
   setup() {
-
-
-    return {}
-  }
-}
+    return {};
+  },
+};
 </script>
 
-<style></style> -->
+<style>
+.logo-slider {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+@keyframes slide {
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-100%);
+  }
+}
+
+.logos {
+  overflow: hidden;
+  padding: 60px 0;
+  background: white;
+  white-space: nowrap;
+  position: relative;
+}
+
+.logos:before,
+.logos:after {
+  position: absolute;
+  top: 0;
+  width: 250px;
+  height: 100%;
+  content: "";
+  z-index: 2;
+}
+
+.logos:before {
+  left: 0;
+  background: linear-gradient(to left, rgba(255, 255, 255, 0), white);
+}
+
+.logos:after {
+  right: 0;
+  background: linear-gradient(to right, rgba(255, 255, 255, 0), white);
+}
+
+.logos:hover .logos-slide {
+  animation-play-state: paused;
+}
+
+.logos-slide {
+  display: inline-block;
+  animation: 35s slide infinite linear;
+}
+
+.logos-slide img {
+  height: 200px;
+  margin: 0 40px;
+}
+</style>
