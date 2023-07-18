400d09bdf25a086d6387d526c29339f6ea11a03a
thiên lê <thienle255@gmail.com>
2023-07-19 01:07:43 +0700

    Update FeedbackSeeder.php

--- database/seeders/FeedbackSeeder.php
+++ database/seeders/FeedbackSeeder.php
@@ -28,14 +28,16 @@ class FeedbackSeeder extends Seeder
                 'created_at' => now(),
                 'updated_at' => now(),
             ],[
-                'content' => 'Diana has been,  and continues to be, my binder of choice for over ten years. In reference to my specialization in 18th century books, her restoration and rebinding is state of the art was perfect!',
+                'content' => 'In forty-three years of antiquarian bookselling, we have come across many old and rare books that are in need of care and restoration. We are very grateful for the loving attention they get from Christian.'
+                ,
                 'pinned' => 3,
                 'author' => 3, 
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'content' => 'Diana has been,  and continues to be, my binder of choice for over ten years. In reference to my specialization in 18th century books, her restoration and rebinding is state of the art was perfect!',
+                'content' => 'Diana has been,  and continues to be, my binder of choice for over ten years. In reference to my specialization in 18th century books, her restoration and rebinding is state of the art was perfect! "
+                ',
                 'pinned' => 4,
                 'author' => 4, 
                 'created_at' => now(),
