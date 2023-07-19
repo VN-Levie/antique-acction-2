a49234a75b38e4b08d8ae8fa4001b4be84568d00
thiên lê <thienle255@gmail.com>
2023-07-20 01:00:35 +0700

    Create News.vue

---
+++ resources/js/Pages/News/News.vue
@@ -0,0 +1,16 @@
+<template>
+  <HomeLayout title="Home"> hihii</HomeLayout>
+</template>
+
+<script>
+import HomeLayout from "@/Layouts/HomeLayout.vue";
+
+export default {
+  setup() {
+    return {};
+  },
+  props: {
+    HomeLayout
+  }
+};
+</script>
