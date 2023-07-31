083f5cd5d30c99fa9585ddf3669037b40e6095be
ThanhChuong1 <thanhchuong1412@gmail.com>
2023-07-31 21:28:16 +0700

    \Products\index.vue

--- resources/js/Pages/Products/index.vue
+++ resources/js/Pages/Products/index.vue
@@ -1,85 +1,40 @@
 
 <template>
-    <Head title="products" />
-    <HomeLayout>
-        <div class="bg-white">
-            <div>
-                <!-- Mobile filter dialog -->
-                <TransitionRoot as="template" :show="mobileFiltersOpen">
-                    <Dialog as="div" class="relative z-40 lg:hidden" @close="mobileFiltersOpen = false">
-                        <TransitionChild as="template" enter="transition-opacity ease-linear duration-300"
-                            enter-from="opacity-0" enter-to="opacity-100"
-                            leave="transition-opacity ease-linear duration-300" leave-from="opacity-100"
-                            leave-to="opacity-0">
-                            <div class="fixed inset-0 bg-black bg-opacity-25" />
-                        </TransitionChild>
+    <div>
 
-                        <div class="fixed inset-0 z-40 flex">
-                            <TransitionChild as="template" enter="transition ease-in-out duration-300 transform"
-                                enter-from="translate-x-full" enter-to="translate-x-0"
-                                leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0"
-                                leave-to="translate-x-full">
-                                <DialogPanel
-                                    class="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl">
-                                    <div class="flex items-center justify-between px-4">
-                                        <h2 class="text-lg font-medium text-gray-900">Filters</h2>
-                                        <button type="button"
-                                            class="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
-                                            @click="mobileFiltersOpen = false">
-                                            <span class="sr-only">Close menu</span>
-                                            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
-                                        </button>
-                                    </div>
+        <Head title="Products" />
+        <HomeLayout>
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center">
+                    <span class="title_services">Products</span>
+                </div>
+                <div class="col-12 d-flex justify-content-center">
+                    <img src="/img/mt-1804-home-divider1.png" alt="divider1" />
+                </div>
+            </div>
+            <div id="index-product" class="mt-4 max-w-[1200px] mx-auto px-2">
+                <div class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
+                    <div v-if="products" v-for="product in products" :key="product.id">
+                        <div :id="`Products${product.id}`"
+                            class="bg-white inline-block rounded hover:shadow-[0_0_10px_3px_rgba(0,0,0,0.15)] cursor-pointer">
+                            <router-link :to="`/item/${product.id}`">
+                                <img class="rounded-t" :src="product.url" :alt="product.name" />
+                                <div id="ProductDetails">
+                                    <span class="flex items-center justify-start gap-3 px-1 pt-1">
+                                        <span class="text-[#FF6674] font-semibold">
+                                            ${{ priceComputed(product.price) }}
+                                        </span>
+                                        <span class="text-black text-sm text-light line-through">
+                                            ${{ oldPriceComputed(product.price) }}
+                                        </span>
 
-                                    <!-- Filters -->
-                                    <form class="mt-4 border-t border-gray-200">
-                                        <h3 class="sr-only">Categories</h3>
-                                        <ul role="list" class="px-2 py-3 font-medium text-gray-900">
-                                            <li v-for="category in categories" :key="category">
-                                                <a :href="category.href" class="block px-2 py-3">{{ category.name }}</a>
-                                            </li>
-                                        </ul>
-
-                                        <Disclosure as="div" v-for="section in filters" :key="section.id"
-                                            class="border-t border-gray-200 px-4 py-6" v-slot="{ open }">
-                                            <h3 class="-mx-2 -my-3 flow-root">
-                                                <DisclosureButton
-                                                    class="flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
-                                                    <span class="font-medium text-gray-900">{{ section.name }}</span>
-                                                    <span class="ml-6 flex items-center">
-                                                        <PlusIcon v-if="!open" class="h-5 w-5" aria-hidden="true" />
-                                                        <MinusIcon v-else class="h-5 w-5" aria-hidden="true" />
-                                                    </span>
-                                                </DisclosureButton>
-                                            </h3>
-                                            <DisclosurePanel class="pt-6">
-                                                <div class="space-y-6">
-                                                    <div v-for="(option, optionIdx) in section.options" :key="option.value"
-                                                        class="flex items-center">
-                                                        <input :id="`filter-mobile-${section.id}-${optionIdx}`"
-                                                            :name="`${section.id}[]`" :value="option.value" type="checkbox"
-                                                            :checked="option.checked"
-                                                            class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
-                                                        <label :for="`filter-mobile-${section.id}-${optionIdx}`"
-                                                            class="ml-3 min-w-0 flex-1 text-gray-500">{{ option.label
-                                                            }}</label>
-                                                    </div>
-                                                </div>
-                                            </DisclosurePanel>
-                                        </Disclosure>
-                                    </form>
-                                </DialogPanel>
-                            </TransitionChild>
-                        </div>
-                    </Dialog>
-                </TransitionRoot>
-
-                <main class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                    <div class="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
-                        <h1 class="text-4xl font-bold tracking-tight text-gray-900">Products</h1>
-
-                        <div class="flex items-center">
-                            <Menu as="div" class="relative inline-block text-left">
+                                        <span class="px-1 relative -top-1.5 text-[FF6674] text-xs font-semibold">
+                                            Extra 5%off
+                                        </span>
+                                    </span>
+                                </div>
+                                <span class=" bg-[#FD374F] text-white text-[9px] font-semibold px-1.5
+                                            rounded-sm">Welcome Deal</span>
                                 <div>
                                     <MenuButton
                                         class="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
